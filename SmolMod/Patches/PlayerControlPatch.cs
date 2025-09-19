3fdea5f877709a1a35dd75982dc05166ae76478c
XtraCube <72575280+XtraCube@users.noreply.github.com>
2025-09-19 17:17:02 -0400

    update compatibility (2023.3.28+)

--- SmolMod/Patches/PlayerControlPatch.cs
+++ SmolMod/Patches/PlayerControlPatch.cs
@@ -5,7 +5,6 @@ namespace SmolMod.Patches;
 [HarmonyPatch(typeof(PlayerControl),nameof(PlayerControl.Start))]
 public static class PlayerControlPatch
 { 
-    // adapted from https://github.com/Among-Us-Modding/Laboratory/blob/main/Laboratory/Player/SizeModifier.cs
     public static void Postfix(PlayerControl __instance)
     {
         __instance.UpdateSize();
